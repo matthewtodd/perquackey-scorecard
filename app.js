fb098463b2e7a0b06388e96b99514a497f495ac3
Matthew Todd <matthew@matthewtodd.org>
2011-12-11 11:58:11 -0800

    Build metamorph-less table.
    I barely know what I'm doing, but it seems there may be a bug in
    metamorph?

--- app.js
+++ app.js
@@ -4,27 +4,51 @@ Perquackey.list = SC.ArrayProxy.create({
   content: []
 });
 
-Perquackey.Word = SC.Object.extend({
-  content: '',
-
-  length: function() {
-    return this.getPath('content.length');
-  }.property('content.length').cacheable()
-});
-
 Perquackey.Field = SC.TextField.extend({
   insertNewline: function() {
     var word = this.get('value');
 
     if (word) {
-      this.get('list').pushObject(Perquackey.Word.create({ content: word }));
+      this.get('list').pushObject(word);
       this.set('value', '');
     }
   }
 });
 
 Perquackey.Table = SC.View.extend({
-  headers: function() {
+  tagName: 'table',
+
+  didInsertElement: function() {
+    this._updateElementInnerHtml();
+  },
+
+  _updateElementInnerHtml: SC.observer(function() {
+    this.$().empty();
+
+    var $thead = this.$('<thead/>');
+    var $tr    = this.$('<tr/>');
+    var headers = this._headers();
+    for (var i=0; i<headers.length; i=i+1) {
+      this.$('<th>%@</th>'.fmt(headers[i])).appendTo($tr);
+    }
+    $tr.appendTo($thead);
+    $thead.appendTo(this.$());
+
+    var $tbody = this.$('<tbody/>');
+    var rows = this._rows();
+    for (var i=0; i<rows.length; i=i+1) {
+      var row = rows[i];
+      var $tr = this.$('<tr/>');
+      for (var j=0; j<row.length; j=j+1) {
+        this.$('<td>%@</td>'.fmt(row[j])).appendTo($tr);
+      }
+      $tr.appendTo($tbody);
+    }
+    $tbody.appendTo(this.$());
+
+  }, 'list.@each'),
+
+  _headers: function() {
     var numerically = function(a, b) { return a - b; }
     var lengths = this.get('list').getEach('length').sort(numerically);
 
@@ -32,46 +56,47 @@ Perquackey.Table = SC.View.extend({
     for (var i=lengths[0]; i<=lengths[lengths.length - 1]; i=i+1) {
       headers.push(i);
     }
-    return SC.ArrayProxy.create({ content: headers });
-  }.property('list.@each').cacheable(),
+    return headers;
+  },
 
-  rows: function() {
-    var words = this.getPath('list');
-    var headers = this.get('headers');
+  _rows: function() {
+    var words = this.get('list');
+    var headers = this._headers();
 
     var table = {};
     words.forEach(function(word) {
-      var length = word.get('length');
-      if(!table[length]) table[length] = SC.ArrayProxy.create({ content: [] });
-      table[length].pushObject(word);
+      var length = word.length;
+      if(!table[length]) table[length] = [];
+      table[length].push(word);
     });
 
     var height = 0;
-    headers.forEach(function(length) {
-      var words = table[length];
-      if (words) {
-        if (words.get('length') > height) {
-          height = words.get('length');
+    for (var i=0; i<headers.length; i=i+1) {
+      var length = headers[i];
+      var column = table[length];
+      if (column) {
+        if (column.length > height) {
+          height = column.length;
         }
       }
-    });
-
-    var rows = SC.ArrayProxy.create({ content: [] });
-    for (var rowNumber=0; rowNumber<height; rowNumber=rowNumber+1) {
-      var row = SC.ArrayProxy.create({ content: [] });
-      headers.forEach(function(length) {
-        var column = table[length];
-        var word;
-        if (column && column.objectAt(rowNumber)) {
-          word = column.objectAt(rowNumber);
-        } else {
-          word = Perquackey.Word.create({ content: '' });
-        }
-        row.pushObject(word);
-      });
-      rows.pushObject({words: row});
     }
 
+    var rows = [];
+    for (var rowNumber=0; rowNumber<height; rowNumber=rowNumber+1) {
+      var row = [];
+      for (var i=0; i<headers.length; i=i+1) {
+        var length = headers[i];
+        var column = table[length];
+        var word;
+        if (column && column[rowNumber]) {
+          word = column[rowNumber];
+        } else {
+          word = '';
+        }
+        row.push(word);
+      };
+      rows.push(row);
+    }
     return rows;
-  }.property('headers').cacheable()
+  }
 });
